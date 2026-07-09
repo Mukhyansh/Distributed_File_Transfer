e84849a19251aa4d54d7f210ffcd48b07ce0131f
Mukhyansh <mukbhateja@gmail.com>
2026-07-09 23:54:01 +0530

    fix1

--- main.c
+++ main.c
@@ -6,8 +6,11 @@
 
 int main(int argc,char* argv[]){
 
+    //System function runs the command on the terminal!
     printf("File Chunking check\n");
     system("./src/Chunking/chunk");
+
+    sleep(2);
 
     printf("File compression check\n");
 
@@ -16,12 +19,16 @@ int main(int argc,char* argv[]){
     system("./src/Compressor/compress src/Chunking/3.txt src/Compressor/3.dat");
     system("./src/Compressor/compress src/Chunking/4.txt src/Compressor/4.dat");
 
+    sleep(2);
+
     printf("File transfer check\n");
 
     system("./src/FileTransfer/client 127.0.0.1 8080 src/Compressor/1.dat");
     system("./src/FileTransfer/client 127.0.0.1 8081 src/Compressor/2.dat");
     system("./src/FileTransfer/client 127.0.0.1 8082 src/Compressor/3.dat");
     system("./src/FileTransfer/client 127.0.0.1 8083 src/Compressor/4.dat");
+
+    sleep(2);
 
     printf("Decompressing check\n");
 
@@ -30,11 +37,15 @@ int main(int argc,char* argv[]){
     system("./src/Compressor/decompress src/FileTransfer/received3.dat src/Chunking/out3.txt");
     system("./src/Compressor/decompress src/FileTransfer/received4.dat src/Chunking/out4.txt");
 
+    sleep(2);
+    
     printf("Reconstructing the file!\n");
 
     system("./src/Chunking/reconstruct");
 
-    printf("Done!\n");
+    sleep(1);
+
+    puts("Done Yay Yippee!!!");
 
     return 0;
 }
